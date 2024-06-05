88ec86a7451cf928844ac9f1e7737df7d1cdf0e3
PlasmaX-115 <83971348+PlasmaX-115@users.noreply.github.com>
2024-06-04 19:20:54 -0600

    E: Or Logical Operator

--- project/delta/codegen.py
+++ project/delta/codegen.py
@@ -90,14 +90,30 @@ class CodeGenerationVisitor(PTNodeVisitor):
     def visit_expression (self, node, children):
         if len(children) == 1:
             return children[0]
+        else:
+            result = []
+            for exp in children[:-1]:
+                result.append(exp)
+                result.append('    if(result i32)\n')
+                result.append('    i32.const 1\n')
+                result.append('    else')
+            result.append(children[-1])
+            result.append('    i32.eqz\n')
+            result.append('    i32.eqz\n')
+            result.append('    end\n' * (len(children) - 1))
+            return ''.join(result)
+        
+    def visit_and_expression(self, node, children):
+        if len(children) == 1:
+            return children[0]
         result = [children[0]]
         for exp in children[1:]:
-            result.append('    if(result i32)\n')
+            result.append('    if (result i32)\n')
             result.append(exp)
         result.append('    i32.eqz\n' * 2)
-        result.append((  '   else\n'
-                      +  '    i32.const 0\n'
-                      +  '    end\n') * (len(children)-1))
+        result.append(('    else\n'
+                      + '    i32.const 0\n'
+                      + '    end\n') * (len(children) - 1))
         return ''.join(result)
 
     def visit_additive(self, node, children):
